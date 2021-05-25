ff039c37f541a405736d3d56e79a34d84e0194f4
Rodrigo Montelongo <rodrigo.montelongo@softtek.com>
2021-05-25 12:08:41 +0200

    Ejercicio IVA

---
+++ iva.js
@@ -0,0 +1,6 @@
+let precio = 1.2;
+const iva = 21;
+let precioIva = precio*(1+iva/100);
+
+console.log('El precio del boligrafo con iva es: '+precioIva+' €');
+console.log('el precio sin iva es: '+precio+' €.');
